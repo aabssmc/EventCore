0606251d56cbcea2e4952b4634d8df270e6b9f75
aabssmc <lilmikeyninja203@gmail.com>
2024-04-27 11:39:01 -0700

    fix Visibility

--- src/main/java/lol/aabss/eventcore/commands/Visibility.java
+++ src/main/java/lol/aabss/eventcore/commands/Visibility.java
@@ -1,24 +1,16 @@
 package lol.aabss.eventcore.commands;
 
-import lol.aabss.eventcore.util.Config;
-import lol.aabss.eventcore.EventCore;
 import lol.aabss.eventcore.util.SimpleCommand;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static lol.aabss.eventcore.EventCore.instance;
 import static lol.aabss.eventcore.util.Config.msg;
-import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 
 public class Visibility implements SimpleCommand {
 
@@ -32,59 +24,64 @@ public class Visibility implements SimpleCommand {
             return true;
         }
         if (args.length == 0){
-            Config.sendMessagePrefix(p, " <red>/visibility <all | staff | off>");
+            p.sendMessage(msg("<red>/visibility <all | staff | off>"));
             return true;
         }
         switch (args[0]) {
-            case "all":
+            case "all" -> {
                 if (p.hasPermission("eventcore.visibility.all")) {
                     if (VisAll.contains(p)) {
                         sender.sendMessage(msg("visibility.allalreadyhidden"));
-                    } else {
-                        for (Player player : Bukkit.getOnlinePlayers()) {
-                            p.hidePlayer(instance, player);
-                        }
-                        VisAll.add(p);
-                        VisStaff.remove(p);
-                        sender.sendMessage(msg("visibility.allhidden"));
+                        return true;
                     }
-                } else {
-                    sender.sendMessage(msg("permission-message"));
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        p.hidePlayer(instance, player);
+                    }
+                    VisAll.add(p);
+                    VisStaff.remove(p);
+                    sender.sendMessage(msg("visibility.allhidden"));
+                    return true;
                 }
-            case "staff":
+                sender.sendMessage(msg("permission-message"));
+                return true;
+            }
+            case "staff" -> {
                 if (p.hasPermission("eventcore.visibility.staff")) {
                     if (VisStaff.contains(p)) {
                         sender.sendMessage(msg("visibility.staffalreadyhidden"));
-                    } else {
-                        for (Player player : Bukkit.getOnlinePlayers()) {
-                            if (!player.hasPermission("eventcore.visibility.staffbypass")) {
-                                p.hidePlayer(instance, player);
-                            }
-                        }
-                        VisAll.remove(p);
-                        VisStaff.add(p);
-                        sender.sendMessage(msg("visibility.staffhidden"));
+                        return true;
                     }
-                } else {
-                    sender.sendMessage(msg("permission-message"));
-                }
-            case "off":
-                if (p.hasPermission("eventcore.visibility.off")) {
-                    if (VisStaff.contains(p) || VisAll.contains(p)) {
-                        for (Player player : Bukkit.getOnlinePlayers()) {
-                            p.showPlayer(instance, player);
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        if (!player.hasPermission("eventcore.visibility.staffbypass")) {
+                            p.hidePlayer(instance, player);
                         }
-                        VisAll.remove(p);
-                        VisStaff.remove(p);
-                        sender.sendMessage(msg("visibility.visibilityoff"));
-                    } else {
-                        sender.sendMessage(msg("visibility.visibilityalreadyoff"));
                     }
-                } else {
-                    sender.sendMessage(msg("permission-message"));
+                    VisStaff.add(p);
+                    VisAll.remove(p);
+                    sender.sendMessage(msg("visibility.staffhidden"));
+                    return true;
                 }
-            default:
-                Config.sendMessagePrefix(p, " <red>/visibility <all | staff | off>");
+                sender.sendMessage(msg("permission-message"));
+                return true;
+            }
+            case "off" -> {
+                if (!p.hasPermission("eventcore.visibility.off")) {
+                    sender.sendMessage(msg("permission-message"));
+                    return true;
+                }
+                if (!VisStaff.contains(p) && !VisAll.contains(p)) {
+                    sender.sendMessage(msg("visibility.visibilityalreadyoff"));
+                    return true;
+                }
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    p.showPlayer(instance, player);
+                }
+                VisAll.remove(p);
+                VisStaff.remove(p);
+                sender.sendMessage(msg("visibility.visibilityoff"));
+                return true;
+            }
+            default -> p.sendMessage(msg("<red>/visibility <all | staff | off>"));
         }
         return true;
     }
